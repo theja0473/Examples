8eb7e1234c319f9af0cd9b9b0c9e37c504a849ed
uMIC.200 User <umic@uMIC.200>
2017-07-03 10:44:46 +0200

    Umdate digital I/O example

--- digital-io/app_dio.c
+++ digital-io/app_dio.c
@@ -1,3 +1,32 @@
+//============================================================================//
+// File:          app_dio.h                                                   //
+// Description:   Example for digital I/O                                     //
+//                                                                            //
+// Copyright (C) MicroControl GmbH & Co. KG                                   //
+// 53844 Troisdorf - Germany                                                  //
+// www.microcontrol.net                                                       //
+//                                                                            //
+//----------------------------------------------------------------------------//
+// Redistribution and use in source and binary forms, with or without         //
+// modification, are permitted provided that the following conditions         //
+// are met:                                                                   //
+// 1. Redistributions of source code must retain the above copyright          //
+//    notice, this list of conditions, the following disclaimer and           //
+//    the referenced file 'LICENSE'.                                          //
+// 2. Redistributions in binary form must reproduce the above copyright       //
+//    notice, this list of conditions and the following disclaimer in the     //
+//    documentation and/or other materials provided with the distribution.    //
+// 3. Neither the name of MicroControl nor the names of its contributors      //
+//    may be used to endorse or promote products derived from this software   //
+//    without specific prior written permission.                              //
+//                                                                            //
+// Provided that this notice is retained in full, this software may be        //
+// distributed under the terms of the GNU Lesser General Public License       //
+// ("LGPL") version 3 as distributed in the 'LICENSE' file.                   //
+//                                                                            //
+//============================================================================//
+
+
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,9 +39,7 @@
 #include <umic_dio.h>
 #include <umic_relay.h>
 
-/*
-** Code Beispiel
-*/
+
 
 int main()
 {
